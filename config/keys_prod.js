4d04c4d8012dac78a586a402085c897b45fdf017
KananHabibli <habiblikanan@gmail.com>
2020-03-26 21:56:57 +0400

    Init commit

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,7 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
     googleClientID : process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    facebookClientID: process.env.FACEBOOK_CLIENT_ID,
+    facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET
 }
