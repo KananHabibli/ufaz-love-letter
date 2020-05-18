bd1ba8dd0636ea05c929f5c77779288aeae31384
KananHabibli <habiblikanan@gmail.com>
2020-05-19 02:32:33 +0400

    Bugs fixed

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(cors())
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://ufaz-love-letter.herokuapp.com/');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
