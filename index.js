64b82a56cec2948fa71ada0a993489613187e371
KananHabibli <habiblikanan@gmail.com>
2020-05-19 02:12:16 +0400

    Access-Control-Allow-Origin added

--- index.js
+++ index.js
@@ -26,25 +26,6 @@ mongoose.set('useCreateIndex', true);
 var cors = require('cors')
 app.use(cors())
 
-// Add headers
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 
 // Built-in node package for working with file and directory paths
 const path = require('path')
